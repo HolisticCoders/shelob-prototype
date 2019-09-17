d37bb68b21b0f82267e715e776f321b61cc87189
Muream <muream@gmail.com>
2019-09-17 23:44:48 +0200

    store nodes in the graph in the same way that attributes are stored on nodes.

--- shelob/core/graph.py
+++ shelob/core/graph.py
@@ -3,9 +3,15 @@ from shelob.core.node import Node
 
 class Graph:
     def __init__(self):
-        self.nodes = []
+        self.nodes = {}
 
-    def add_node(self):
-        new_node = Node()
-        self.nodes.append(new_node)
+    def add_node(self, name):
+        if name in self.nodes:
+            raise RuntimeError(f"A Node with the name {name} already exists")
+        new_node = Node(name)
+        self.nodes[name] = new_node
 
+    def delete_node(self, name):
+        if name not in self.nodes:
+            raise RuntimeError(f"Node {name} does not exist and cannot be deleted")
+        del self.nodes[name]
--- tests/core/test_graph.py
+++ tests/core/test_graph.py
@@ -1,3 +1,5 @@
+import pytest
+
 from shelob.core.graph import Graph
 
 
@@ -7,6 +9,26 @@ def test_create_graph():
 
 def test_add_node():
     graph = Graph()
-    graph.add_node()
+    graph.add_node("MyNode")
     assert len(graph.nodes) == 1
+
+
+def test_add_conflicting_node():
+    graph = Graph()
+    graph.add_node("MyNode")
+    with pytest.raises(RuntimeError):
+        graph.add_node("MyNode")
+
+
+def test_delete_node():
+    graph = Graph()
+    graph.add_node("MyNode")
+    graph.delete_node("MyNode")
+    assert len(graph.nodes) == 0
+
+
+def test_delete_inexistant_node():
+    graph = Graph()
+    with pytest.raises(RuntimeError):
+        graph.delete_node("MyNode")
 
